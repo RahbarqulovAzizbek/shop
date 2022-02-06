d27c07348aeeb2e7a639e3fa2c4cba5959d17226
Azizbek <azizbekrakhbarkulov@gmail.com>
2022-02-06 22:49:01 +0500

    models have added

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog', include('blog.urls', namespace='blogs')),
     path('', include('pages.urls', namespace='pages'))
 ]
 
